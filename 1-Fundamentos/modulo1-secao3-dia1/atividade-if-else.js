f0ca64dd66b88273bb16e22b749d138306c71656
Fernanda <fernanda_petrachim@hotmail.com>
2023-03-28 22:54:51 -0300

    arividade de explicação2

---
+++ 1-Fundamentos/modulo1-secao3-dia1/atividade-if-else.js
@@ -0,0 +1,16 @@
+let nota = 100;
+if (nota === 100)
+{
+console.log ('Parabéns você passo!');
+}else if (nota < 100 && nota > 80 ){
+
+console.log('Parabéns, você faz parte do grupo de pessoas aprovadas');
+}else if (nota < 100 && nota > 80){ 
+
+console.log ('Parabéns você tirou C');
+}else if (nota < 80 && nota > 60){
+console.log ("Você está na nossa lista de espera")
+}else if (nota <60 && nota >60){
+console.log("Infelizmente, você reprovou.")
+}
+
