602fe6c66490bab3bd3aa6a168dd09770a8be37c
Fernanda <fernanda_petrachim@hotmail.com>
2023-03-30 17:50:41 -0300

    pasta nova

---
+++ 1-Fundamentos/modulo1-secao3-dia3/praticar.funcoes.js
@@ -0,0 +1,36 @@
+let saldo = 1500;
+
+function somarSaldo(saldo, somar){
+return saldo + somar;    
+}
+saldo = somarSaldo(saldo, 200)
+console.log (saldo);
+
+function subtrairValor(saldo, subtrair){
+return saldo - subtrair;
+}
+saldo = subtrairValor(saldo, 500)
+console.log (saldo);
+
+function multiplicarValor(saldo, multiplicar){
+return saldo * multiplicar;
+}
+saldo = multiplicarValor (saldo, 300)
+console.log(saldo);
+
+function dividirValor(saldo, dividir){
+return saldo / dividir;
+}
+saldo = dividirValor(saldo, 400)
+console.log(saldo);
+
+
+
+
+
+
+
+
+
+
+
